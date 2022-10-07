7d57fa855c8efdc7702496cb779598e2b21b9f33
vfj-tanaka <108640688+vfj-tanaka@users.noreply.github.com>
2022-10-07 10:25:11 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by mtanaka on 2022/10/07.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

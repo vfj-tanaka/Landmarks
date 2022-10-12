abcc69caa736451e94e55682396ba95d392952e6
vfj-tanaka <108640688+vfj-tanaka@users.noreply.github.com>
2022-10-12 17:15:05 +0900

    section~5

--- Landmarks/View/LandmarkRow.swift
+++ Landmarks/View/LandmarkRow.swift
@@ -27,6 +27,8 @@ struct LandmarkRow: View {
 }
 
 struct LandmarkRow_Previews: PreviewProvider {
+    
+    static var landmarks = ModelData().landmarks
     static var previews: some View {
         LandmarkRow(landmark: landmarks[0])
         LandmarkRow(landmark: landmarks[1])
